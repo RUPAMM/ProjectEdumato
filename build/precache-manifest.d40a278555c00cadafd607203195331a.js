d65826f322950ce6d857d6f74c83f306f30772a7
Rupam Mankar <52576507+RUPAMM@users.noreply.github.com>
2020-11-10 15:11:09 +0530

    Add files via upload
    This is the repo of online food ordering system by react js, mongodb, node js.

---
+++ build/precache-manifest.d40a278555c00cadafd607203195331a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a96122f52f06602b1997b5dac8f6dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78eb3069581e8f0df857",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "bdb18d2a4af5e4188acd",
+    "url": "/static/css/main.078af693.chunk.css"
+  },
+  {
+    "revision": "78eb3069581e8f0df857",
+    "url": "/static/js/2.5ebe814d.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/static/js/2.5ebe814d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb18d2a4af5e4188acd",
+    "url": "/static/js/main.4c55ef97.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  }
+]);
